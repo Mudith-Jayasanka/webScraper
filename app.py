48060f2267e7270f128e0c4e3be7d671691cbf7c
Mudith-Jayasanka <mudith.jayasanka@gmail.com>
2021-05-12 13:56:21 +0530

    jsonify removed

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ flask_cors.CORS(app)
 @app.route("/reset",methods=["GET"])
 def resetScraper():
     myScraper.reset()
-    return jsonify("[SCRAPER]-Reset")
+    return "[SCRAPER]-Reset"
 
 @app.route("/getNext" , methods=["GET"])
 def getNext():
